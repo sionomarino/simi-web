94175328baad6ed7291c7a6d29b7da507b573a7a
SIMI <thiagoagustingomez08@gmail.com>
2025-08-03 22:09:29 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,6 @@
-// supabase-config.js
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = "https://cbngdegxmfzkstistitg.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNibmdkZWd4bWZ6a3N0aXN0aXRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyNTczMDQsImV4cCI6MjA2OTgzMzMwNH0.0wxaV19YNRrnXiFbtGIKgr9v32pwSQSbwRaWnyd3SlQ";
+const supabaseUrl = 'https://cbngdegxmfzkstistitg.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNibmdkZWd4bWZ6a3N0aXN0aXRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyNTczMDQsImV4cCI6MjA2OTgzMzMwNH0.0wxaV19YNRrnXiFbtGIKgr9v32pwSQSbwRaWnyd3SlQ'
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey)
